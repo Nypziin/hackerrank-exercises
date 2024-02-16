019adba227b1275f89be905f8bcb7c96d4880782
Natan Cardoso <Natan>
2024-02-15 21:21:23 -0300

    improving the code

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ if __name__ == '__main__':
         item_name = ' '.join(item[:-1])
         net_price = int(item[-1])
 
-        if item_name in dicionario:
-            dicionario[item_name] += net_price
-        else:
-            dicionario[item_name] = net_price
+        dicionario[item_name] = dicionario.get(item_name, 0) + net_price
 
     for item, price in dicionario.items():
         print(f'{item} {price}')
