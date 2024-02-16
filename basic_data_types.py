cb1464f39dd719a61cf051d2e33bbfac6fc7b056
Natan Cardoso <Natan>
2024-02-15 21:40:37 -0300

    exercise: Basic Data Types

--- basic_data_types.py
+++ basic_data_types.py
@@ -4,14 +4,7 @@ if __name__ == '__main__':
     z = int(input())
     n = int(input())
 
-    lista = []
-
-    for a in range(x + 1):
-        for b in range(y + 1):
-            for c in range(z + 1):
-                if a + b + c != n:
-                    lista_temporaria = [a, b, c]
-                    lista.append(lista_temporaria)
+    lista = [[a, b, c] for a in range(x + 1) for b in range(y + 1) for c in range(z + 1) if a + b + c != n]
 
     print(lista)
 
